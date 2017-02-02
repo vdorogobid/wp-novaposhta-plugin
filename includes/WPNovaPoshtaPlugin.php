a13bbe82bdc69c501651bcd9d678965783698005
vdorogobid <vdorogobid@gmail.com>
2017-02-02 23:14:35 +0200

    Класс плагина

---
+++ includes/WPNovaPoshtaPlugin.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: romansolomashenko
+ * Date: 16.01.17
+ * Time: 6:53 PM
+ */
+namespace includes;
+
+class WPNovaPoshtaPlugin
+{
+    private static $instance = null;
+    private function __construct() {
+
+    }
+    public static function getInstance() {
+
+        if ( null == self::$instance ) {
+            self::$instance = new self;
+        }
+
+        return self::$instance;
+
+    }
+
+    static public function activation()
+    {
+        // debug.log
+        error_log('plugin '.WPNOVAPOSHTA_PlUGIN_NAME.' activation');
+    }
+
+    static public function deactivation()
+    {
+        // debug.log
+        error_log('plugin '.WPNOVAPOSHTA_PlUGIN_NAME.' deactivation');
+    }
+
+}
+
+WPNovaPoshtaPlugin::getInstance();
