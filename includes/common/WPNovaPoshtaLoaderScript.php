8904c0fe329ce0438d5da461b80d17a53e667be4
vdorogobid <vdorogobid@gmail.com>
2017-02-10 15:24:17 +0200

    Класс отвечающий за добавление JS и CSS в плагине

---
+++ includes/common/WPNovaPoshtaLoaderScript.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: romansolomashenko
+ * Date: 05.02.17
+ * Time: 5:02 PM
+ */
+
+namespace includes\common;
+
+
+class WPNovaPoshtaLoaderScript
+{
+    
+
+    }
+
+   
