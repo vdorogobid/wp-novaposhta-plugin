6608e73d7d63638b2194f52f4e8e4db52c39f38f
vdorogobid <vdorogobid@gmail.com>
2017-02-16 22:54:25 +0200

    Метод проверяющий наличие настроек если их нет создает по умолчанию

--- includes/WPNovaPoshtaPlugin.php
+++ includes/WPNovaPoshtaPlugin.php
@@ -8,6 +8,7 @@
  */
 namespace includes;
 
+use includes\common\WPNovaPoshtaDefaultOption;
 use includes\common\WPNovaPoshtaLoader;
 
 
@@ -16,6 +17,7 @@ class WPNovaPoshtaPlugin
     private static $instance = null;
     private function __construct() {
         WPNovaPoshtaLoader::getInstance();
+        add_action('plugins_loaded', array(&$this, 'setDefaultOptions'));
     }
     public static function getInstance() {
 
@@ -27,6 +29,18 @@ class WPNovaPoshtaPlugin
 
     }
 
+     /**
+      * Если не созданные настройки установить по умолчанию
+      */
+    public function setDefaultOptions(){
+         if( ! get_option(WPNOVAPOSHTA_PlUGIN_OPTION_NAME) ){
+            update_option( WPNOVAPOSHTA_PlUGIN_OPTION_NAME, WPNovaPoshtaDefaultOption::getDefaultOptions() );
+         }
+         if( ! get_option(WPNOVAPOSHTA_PlUGIN_OPTION_VERSION) ){
+             update_option(WPNOVAPOSHTA_PlUGIN_OPTION_VERSION, WPNOVAPOSHTA_PlUGIN_VERSION);
+         }
+     }
+    
     static public function activation()
     {
         // debug.log
